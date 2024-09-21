2f0791223837d8ad2ebbd5004ec23ff3a0a08506
anantakumarghosh <antaghosh@gmail.com>
2024-09-21 15:02:52 +0530

    fix(core): :bug: this is a test
    this is a test, update package-lock.json
    
    ref: #7

--- app/components/ModuleComponent.js
+++ app/components/ModuleComponent.js
@@ -5,7 +5,7 @@ import ModuleClasses from "../styles/ModuleClasses";
 export default function ModuleComponent() {
   return (
     <CoreBox styleClasses={[CoreClasses.PADDING.P5, ModuleClasses.TEST_WRAPPID_STYLE_CLASS]}>
-      <CoreTypographyBody1>ModuleComponent</CoreTypographyBody1>
+      <CoreTypographyBody1>ModuleComponent test</CoreTypographyBody1>
     </CoreBox>
   );
 }
